9249929a7b4a94fabe3c12aee809659788f93929
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-20 20:06:53 +0100

    Add assessment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+class Main {
+
+    public static String KUniqueCharacters(String str) {
+        // code goes here
+        return str;
     }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(KUniqueCharacters(s.nextLine()));
+    }
+
 }
+
